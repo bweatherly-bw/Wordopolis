766887fde08ba13ee0733e02d61309ec51d0b272
bweatherly-bw <brandon.weatherly58@gmail.com>
2021-07-26 18:56:30 -0400

    Created a ladning page for the application that the user is able to interace with before the application. Also added a JS library to part of the landing page along with the main Dictionary application. Will need to create an animation for the ENTER on the lading page for a nice page transition to the application.

---
+++ landingpage.js
@@ -0,0 +1,7 @@
+let enterPage = u(document.getElementById('enter_button'));
+enterPage.text('ENTER');
+
+let title = u(document.querySelector('h1'));
+title.text('Dictionary Application');
+
+
--- script.js
+++ script.js
@@ -7,20 +7,26 @@ body.appendChild(wordContainer);
 wordContainer.appendChild(word); 
 wordContainer.appendChild(definition); 
 
-const randomWord = () => {
+let headings = u(document.getElementsByClassName('title'))
+headings.html('Dictionary API');
+
+let user_text = u(document.getElementsByClassName('text_for_user'))
+user_text.html('Push the button to receive a new word!');
+
+let button_ = u(document.getElementsByClassName('button'));
+button_.html('ENTER');
+
+let randomWord = () => {
     fetch('https://random-word-api.herokuapp.com/word?number=1')
     .then(response => {
-        return response.json(); 
+        console.log('response');
+        return response.json();
     })
     .then(response => {
         word.textContent = response
         randomDefinition(word);
-        wordContainer.classList.remove('display-none'); 
+        wordContainer.classList = "No Word Available!"
     })
-    .catch(err => {
-        console.log(err); 
-        word.textContent = "No Word Available"
-    }); 
 }
 
 const randomDefinition = (word) => {

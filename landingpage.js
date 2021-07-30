d4b0206d902da1d78d504e666e1116b70704394e
bweatherly-bw <brandon.weatherly58@gmail.com>
2021-07-30 03:42:41 -0400

    corrected the issue with the API key returning a 404 error on the website. Cleaned up some extra unnecesary code.

--- landingpage.js
+++ landingpage.js
@@ -4,4 +4,9 @@ enterPage.text('LAUNCH');
 let title = u(document.querySelector('h1'));
 title.text('WORDOPOLIS');
 
+let image = u(document.querySelector('img'));
 
+// var test = document.getElementById('enter_button');
+// test.onclick = () => {
+//     console.log('testing');
+// }
